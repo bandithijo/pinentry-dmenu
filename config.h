56a6c98dac845a3466aec7a0a7caa59326636fe7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-10-30 19:13:57 +0800

    Change mon from -1 to 0

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int bottom = 0;
+static int bottom   = 0;
 static int embedded = 0;
 static int minpwlen = 10;
-static int mon = -1;
+static int mon      = 0;
 
 static const char *asterisk = "#";
 static const char *fonts[] = {
